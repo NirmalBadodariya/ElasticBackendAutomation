17f9d4850368eec5326ad89b056824d4e90f9d16
NirmalBadodariya <nirmal.inexture@gmail.com>
2022-09-12 17:09:33 +0530

    monday work with get apis automation

--- src/test/java/ElasticSI_Project.java
+++ src/test/java/ElasticSI_Project.java
@@ -3,10 +3,8 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
+import java.util.*;
+import java.util.regex.*;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
@@ -17,6 +15,8 @@ import io.restassured.response.ResponseBody;
 import io.restassured.specification.RequestSpecification;
 import net.minidev.json.parser.ParseException;
 import org.apache.http.HttpStatus;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -42,7 +42,7 @@ public class ElasticSI_Project {
 		Response resFromGenrateedToken = request.body(payload).post("/authenticate");
 		int statusCode = resFromGenrateedToken.getStatusCode();
 		System.out.println(statusCode);
-		Assert.assertEquals(statusCode /*actual value*/, 200 /*expected value*/,
+		Assert.assertEquals(statusCode /*actual value*/, 400 /*expected value*/,
 				"wrong email entered");
 
 	}
@@ -62,7 +62,7 @@ public class ElasticSI_Project {
 		Response resFromGenrateedToken = request.body(payload).post("/authenticate");
 		int statusCode = resFromGenrateedToken.getStatusCode();
 		System.out.println(statusCode);
-		Assert.assertEquals(statusCode /*actual value*/, 200 /*expected value*/,
+		Assert.assertEquals(statusCode /*actual value*/, 400 /*expected value*/,
 				"wrong password entered");
 
 	}
@@ -102,9 +102,9 @@ public class ElasticSI_Project {
 		request.header("content-type","application/json;charset=UTF-8");
 		request.header("accept","application/json, text/plain, */*");
 
-		Response resFromGenrateedToken = request.body(payload).post("/authenticate");
+		Response resFromGeneratedToken = request.body(payload).post("/authenticate");
 
-		String jsonString = resFromGenrateedToken.getBody().asString();
+		String jsonString = resFromGeneratedToken.getBody().asString();
 
 
 		System.out.println("JSON String is :"+ jsonString);
@@ -149,23 +149,47 @@ public class ElasticSI_Project {
 			}
 
 		};
+
 		String result = given()
 				.headers(headers)
 				.get("search-admin/api/projects")
 				.andReturn().asString();
 		System.out.println(result);
 
+		JSONObject obj = new JSONObject(result);
+
 
 		JsonPath jsonPathEvaluator = new JsonPath(result);
+		List allFields = new ArrayList();
 
-		ArrayList a = jsonPathEvaluator.getJsonObject("result.aiml_enabled");
-		for(Object arr :a)
+			JSONArray array = obj.getJSONArray("result");
+
+			Iterator<Object> iterator = array.iterator();
+			while(iterator.hasNext()){
+				JSONObject jsonObject = (JSONObject) iterator.next();
+				for(String key : jsonObject.keySet()){
+					allFields.add(key);
+				}
+			}
+		System.out.println(allFields);
+		System.out.println(allFields.get(2));
+		ArrayList getData = jsonPathEvaluator.getJsonObject("result."+allFields.get(2));
+		String dataOfFirst = getData.get(0).toString();
+
+		System.out.println(dataOfFirst);
+		String numRegex   = ".*[0-9].*";
+		String alphaRegex = ".*[a-z].*";
+//		if (!(dataOfFirst.matches(numRegex) && dataOfFirst.matches(alphaRegex))) {
+//			Assert.assertEquals(false, true,"doesn't meet the expected result");
+//		}
+
+		for(Object arr :getData)
 		{
 			if(arr!=null)
 			{
 				String dataType =arr.getClass().getSimpleName();
 				System.out.println(arr + " " + arr.getClass().getSimpleName());
-				if (!dataType.equals("String")){
+				if (!dataType.equals("String") && !dataType.equals("Boolean") ){
 					Assert.assertEquals(true, false,"Not String");
 				}
 			}
@@ -193,16 +217,15 @@ public class ElasticSI_Project {
 				.get("search-admin/api/projects").then()
 				.body("$", hasKey("code"))
 				.body("$", hasKey("result"))
-				.body("$",hasKey("message"))
-				.body("result.status",hasItemInArray("PUBLISHED"))
-				.body(containsString("version")).toString();
+				.body("$",hasKey("message"));
+
 
 				
 
 	}
 	@Test
 	public void getProjectsOptimized() throws ParseException {
-		
+
 		Map<String ,String> headers =new HashMap<String,String>(){
 			
 			private static final long serialVersionUID = 1L;
@@ -217,7 +240,7 @@ public class ElasticSI_Project {
 		given()
 		.headers(headers)
 		.when()
-		.get("https://search-admin-dev-mamb5phriq-uc.a.run.app/search-admin/api/projects/projectListing")
+		.get("search-admin/api/projects/projectListing")
 		.then()
 		.assertThat()
 		.statusCode(HttpStatus.SC_OK).log().all();
@@ -267,7 +290,7 @@ public class ElasticSI_Project {
 				.get("search-admin/api/projects/"+id)
 				;
 
-		Assert.assertEquals(response.getStatusCode() /*actual value*/, 200 /*expected value*/,
+		Assert.assertEquals(response.getStatusCode() /*actual value*/, 404 /*expected value*/,
 				"No projects with this id");
 
 	}
@@ -284,11 +307,11 @@ public class ElasticSI_Project {
 				put("Authorization","Bearer "+ getToken());
 			}	
 		};
-		
+
 		given()
 		.headers(headers)
 		.when()
-		.get("https://search-admin-dev-mamb5phriq-uc.a.run.app/search-admin/api/projects/53ca594d-c13b-4367-867f-6c3af8a74c09/details")
+		.get("search-admin/api/projects/53ca594d-c13b-4367-867f-6c3af8a74c09/details")
 		.then()
 		.assertThat()
 		.statusCode(HttpStatus.SC_OK)
@@ -296,7 +319,8 @@ public class ElasticSI_Project {
 
 	}
 	
-	
+
+//	CANT WORK RIGHT NOW
 	@Test
 	public void getExtension() throws ParseException {
 		
@@ -313,7 +337,7 @@ public class ElasticSI_Project {
 	given()
 		.headers(headers)
 		.when()
-			.get("https://search-admin-dev-mamb5phriq-uc.a.run.app/search-admin/api/templates/extension")
+			.get("search-admin/api/templates/extension")
 		.then()
 		.assertThat()
 		.statusCode(HttpStatus.SC_OK)
@@ -332,7 +356,7 @@ public class ElasticSI_Project {
 	public void getFilterDetails() throws ParseException {
 		
 		Map<String ,String> headers =new HashMap<String,String>(){
-			
+
 			private static final long serialVersionUID = 1L;
 
 			{
@@ -340,11 +364,11 @@ public class ElasticSI_Project {
 				put("Authorization","Bearer "+ getToken());
 			}	
 		};
-		
+		String id = "7ccd9bac-821e-462a-aeef-d1a80bc58633";
 		given()
 		.headers(headers)
 		.when()
-		.get("https://search-admin-dev-mamb5phriq-uc.a.run.app/search-admin/api/projects/6506c7f3-b3f0-4c78-853d-053d253e1dee/filtersDetails")
+		.get("search-admin/api/projects/"+id+"/filtersDetails")
 		.then()
 		.assertThat()
 		.statusCode(HttpStatus.SC_OK)
@@ -364,17 +388,53 @@ public class ElasticSI_Project {
 				put("Authorization","Bearer "+ getToken());
 			}	
 		};
-		
+		String id = "7ccd9bac-821e-462a-aeef-d1a80bc58633";
 		given()
 		.headers(headers)
 		.when()
-		.get("https://search-admin-dev-mamb5phriq-uc.a.run.app/search-admin/api/projects/6506c7f3-b3f0-4c78-853d-053d253e1dee/filtersDetails")
+		.get("search-admin/api/projects/"+id+"/synonymsList")
 		.then()
 		.assertThat()
 		.statusCode(HttpStatus.SC_OK)
 		.log().all();
 
 	}
-	
-	
+	@Test
+	public void settingsDetails() throws ParseException {
+		Map<String ,String> headers =new HashMap<String,String>(){
+
+			private static final long serialVersionUID = 1L;
+
+			{
+				put("Accept","*/*");
+				put("Authorization","Bearer "+ getToken());
+			}
+		};
+
+
+
+		String result = given()
+				.headers(headers)
+				.get("search-admin/api/settings/settingsDetails")
+				.andReturn().asString();
+		System.out.println(result);
+
+
+		JsonPath jsonPathEvaluator = new JsonPath(result);
+
+		ArrayList a = jsonPathEvaluator.getJsonObject("result.id");
+		String idOfProject = (String) a.get(0);
+		ArrayList aa = jsonPathEvaluator.getJsonObject("result.rules_engine_enabled");
+
+		boolean ruleEngine = (boolean) aa.get(0);
+		String numRegex   = ".*[0-9].*";
+		String alphaRegex = ".*[a-z].*";
+
+		if (!(idOfProject.matches(numRegex) && idOfProject.matches(alphaRegex))) {
+			Assert.assertFalse(true, "doesn't meet the expected result");
+		}
+		if (("true".equals(ruleEngine) || "false".equals(ruleEngine))){
+			Assert.assertFalse(true, "doesn't meet the expected result");
+		}
+	}
 }
